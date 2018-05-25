71a22bde906b1d88605e712450eb1dbf690fce5a
codepanda <17920082@qq.com>
2018-05-25 08:46:37 +0800

    rewrite app euipment model

--- mnolms/equipment/admin.py
+++ mnolms/equipment/admin.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
+
+from .models import Equipment
 from .models import Manufacturer
 from .models import SensorModel
-from .models import Sensor
+from .models import SensorEntity
 from .models import DataloggerModel
-from .models import Datalogger
+from .models import DataloggerEntity
+from .models import GPSAntenna
 from .models import PowerSupply
-from .models import Equipment
+from .models import NetworkDevice
 from .models import Category
 
-admin.site.register(Manufacturer)
+
+# admin.site.register(Manufacturer)
 admin.site.register(SensorModel)
-admin.site.register(Sensor)
+admin.site.register(SensorEntity)
 admin.site.register(DataloggerModel)
-admin.site.register(Datalogger)
+admin.site.register(DataloggerEntity)
+admin.site.register(GPSAntenna)
 admin.site.register(PowerSupply)
-admin.site.register(Equipment)
-admin.site.register(Category)
+admin.site.register(NetworkDevice)
+# admin.site.register(Category)
--- mnolms/equipment/models.py
+++ mnolms/equipment/models.py
@@ -1,24 +1,20 @@
 from django.db import models
 from django.contrib.contenttypes.fields import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.fields import GenericRelation
+# from django.contrib.contenttypes.fields import GenericForeignKey
+# from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Manufacturer(models.Model):
     """
     制造商信息
     """
-    name = models.CharField(max_length=64, verbose_name="制造商名称")
+    name = models.CharField(max_length=64, unique=True, verbose_name="制造商名称")
     address = models.CharField(max_length=128,
                                null=True,
                                blank=True,
                                verbose_name="地址")
-    zip_code = models.CharField(max_length=12,
-                                null=True,
-                                blank=True,
-                                verbose_name="邮编")
+    remark = models.TextField(null=True, blank=True, verbose_name="备注")
     is_delete = models.BooleanField(default=False, verbose_name="已删除")
-    remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="描述")
 
     def __str__(self):
         return self.name
@@ -30,23 +26,29 @@ class Manufacturer(models.Model):
 
 class Equipment(models.Model):
     """
+    所有设备的共有字段
     设备信息
     """
     category = models.ForeignKey(
         "Category",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         related_name="设备")
-    name = models.CharField(max_length=128, verbose_name="设备名称")
-    # manufacturer = models.ForeignKey("Manufacturer",
-    #                                  null=True,
-    #                                  blank=True,
-    #                                  on_delete=models.SET_NULL,
-    #                                  verbose_name="制造商")
-    content_type = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.SET_NULL)
-    object_id = models.PositiveIntegerField(blank=True, null=True)
-    content_object = GenericForeignKey()
+    # name = models.CharField(max_length=128, verbose_name="设备名称")
+    manufacturer = models.ForeignKey("Manufacturer",
+                                     null=True,
+                                     blank=True,
+                                     on_delete=models.DO_NOTHING,
+                                     verbose_name="制造商")
+
+    totality = models.PositiveIntegerField(default=0, verbose_name="总数")
+    stock = models.PositiveIntegerField(default=0, verbose_name="库存")
+
+    remark = models.TextField(null=True, blank=True, verbose_name="备注")
+    is_delete = models.BooleanField(default=False, verbose_name="已删除")
+
+    # content_type = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.SET_NULL)
+    # object_id = models.PositiveIntegerField(blank=True, null=True)
+    # content_object = GenericForeignKey()
 
 
 class Category(models.Model):
@@ -54,30 +56,33 @@ class Category(models.Model):
     设备分类
     """
     name = models.CharField(max_length=128, verbose_name="分类名称")
+    remark = models.TextField(null=True, blank=True, verbose_name="备注")
+
+    is_delete = models.BooleanField(default=False, verbose_name="已删除")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "设备分类"
+        verbose_name_plural = "设备分类"
 
 
-class SensorModel(models.Model):
+class SensorModel(Equipment):
     """
     地震仪型号
     """
     name = models.CharField(max_length=64, verbose_name="地震仪型号")
-    manufacturer = models.ForeignKey("Manufacturer",
-                                     null=True,
-                                     blank=True,
-                                     on_delete=models.SET_NULL,
-                                     verbose_name="制造商")
     features = models.CharField(max_length=64,
                                 null=True,
                                 blank=True,
                                 verbose_name="特征字符")
-    remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
 
-    equipment = GenericRelation(Equipment)
-    is_delete = models.BooleanField(default=False, verbose_name="已删除")
+    # equipment = models.OneToOneField("Equipment", on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return "{manufacturer}-{model}-{features}".format(
-            manufacturer=self.manufacturer.name,
+            manufacturer=self.manufacturer,
             model=self.name,
             features=self.features
         )
@@ -87,120 +92,46 @@ class SensorModel(models.Model):
         verbose_name_plural = "地震仪型号"
 
 
-# class SensorFeatures(models.Model):
-#     """
-#     地震仪器特征
-#     """
-#     name = models.CharField(max_length=64, verbose_name="地震仪特征")
-#     sensor_model = models.ForeignKey("SensorModel",
-#                                      null=True,
-#                                      blank=True,
-#                                      on_delete=models.SET_NULL,
-#                                      verbose_name="地震仪型号")
-#     remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
-
-
-class DataloggerModel(models.Model):
+class DataloggerModel(Equipment):
     """
     数采型号
     """
     name = models.CharField(max_length=64, verbose_name="数采型号")
-    manufacturer = models.ForeignKey("Manufacturer",
-                                     null=True,
-                                     blank=True,
-                                     on_delete=models.SET_NULL,
-                                     verbose_name="制造商")
     features = models.CharField(max_length=64,
                                 null=True,
                                 blank=True,
                                 verbose_name="特征字符")
-    remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
-    is_delete = models.BooleanField(default=False, verbose_name="已删除")
 
-    equipment = GenericRelation(Equipment)
+    # equipment = models.OneToOneField("Equipment", on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return "{manufacturer}-{name}-{features}".format(
+            manufacturer=self.manufacturer,
+            name=self.name,
+            features=self.features
+        )
 
-# class DataloggerFeatures(models.Model):
-#     """
-#     数采特征
-#     """
-#     name = models.CharField(max_length=64, verbose_name="数采特征")
-#     datalogger_model = models.ForeignKey("DataloggerModel",
-#                                          null=True,
-#                                          blank=True,
-#                                          on_delete=models.SET_NULL,
-#                                          verbose_name="数采型号")
-#     remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
+    class Meta:
+        verbose_name = "数采型号"
+        verbose_name_plural = "数采型号"
 
 
-# class DataloggerSamplingRate(models.Model):
-#     rate = models.IntegerField(verbose_name="采样率")
-#     datalogger_features = models.ForeignKey("DataloggerFeatures", null=True, blank=True, on_delete=models.SET_NULL, verbose_name="数采特征")
-#     remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
-
-
-class Sensor(models.Model):
+class GPSAntenna(Equipment):
     """
-    测震仪信息
+    GPS天线
     """
-    SENSOR_STATUS = (
-        (1, "库存"),
-        (2, "在线"),
-        (3, "故障"),
-        (4, "维修"),
-        (5, "报废"),
-        (6, "归还")
-    )
-    code = models.CharField(max_length=32, verbose_name="编号")
-    model = models.ForeignKey("SensorModel",
-                              null=True,
-                              blank=True,
-                              on_delete=models.SET_NULL,
-                              verbose_name="地震仪型号")
-    # features = models.ForeignKey("SensorFeatures",
-    #                              null=True,
-    #                              blank=True,
-    #                              on_delete=models.SET_NULL,
-    #                              verbose_name="地震仪特征")
-    status = models.PositiveSmallIntegerField(choices=SENSOR_STATUS,
-                                              default=0,
-                                              verbose_name="状态")
+    name = models.CharField(max_length=64, unique=True, verbose_name="型号")
+    # equipment = models.OneToOneField(Equipment, on_delete=models.DO_NOTHING)
 
-    is_delete = models.BooleanField(default=False, verbose_name="已删除")
-    remark = models.CharField(max_length=256, blank=True, null=True, verbose_name="备注")
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "GPS天线型号"
+        verbose_name_plural = "GPS天线型号"
 
 
-class Datalogger(models.Model):
-    """
-    数采信息
-    """
-    DATALOGGER_STATUS = (
-        (1, "库存"),
-        (2, "在线"),
-        (3, "故障"),
-        (4, "维修"),
-        (5, "报废"),
-        (6, "归还")
-    )
-    code = models.CharField(max_length=32, verbose_name="编号")
-    model = models.ForeignKey("DataloggerModel",
-                              null=True,
-                              blank=True,
-                              on_delete=models.SET_NULL,
-                              verbose_name="数采型号")
-    # features = models.ForeignKey("DataloggerFeatures",
-    #                              null=True,
-    #                              blank=True,
-    #                              on_delete=models.SET_NULL,
-    #                              verbose_name="数采特征")
-    status = models.PositiveSmallIntegerField(choices=DATALOGGER_STATUS,
-                                              default=0,
-                                              verbose_name="状态")
-    is_delete = models.BooleanField(default=False, verbose_name="已删除")
-    remark = models.CharField(max_length=256, blank=True, null=True, verbose_name="备注")
-
-
-class PowerSupply(models.Model):
+class PowerSupply(Equipment):
     """
     供电系统
     """
@@ -216,49 +147,128 @@ class PowerSupply(models.Model):
         (SOLAR_ENERGER, "太阳能"),
         (OTHER, "其它")
     )
-    model = models.CharField(max_length=64, verbose_name="型号")
-    manufacturer = models.ForeignKey("Manufacturer",
-                                     null=True,
-                                     blank=True,
-                                     on_delete=models.SET_NULL,
-                                     verbose_name="制造商")
-    number = models.IntegerField(default=0, verbose_name="数量")
-    inventory = models.IntegerField(default=0, verbose_name="库存")
+    name = models.CharField(max_length=64, unique=True, verbose_name="型号")
+
     type = models.CharField(choices=POWERSUPPLY_TYPE,
                             max_length=2, default=SMART_POWER,
                             verbose_name="电源类型")
-    is_delete = models.BooleanField(default=False, verbose_name="已删除")
-    remark = models.CharField(max_length=256, null=True, blank=True, verbose_name="备注")
 
-    equipment = GenericRelation(Equipment)
+    # equipment = models.OneToOneField(Equipment, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return "{power_type}-{manufacturer}/{model}".format(
-            power_type=self.type,
-            manufacturer=self.manufacturer.name,
-            model=self.model)
+        return "[{type}] {manufacturer}-{name}".format(
+            type=self.type,
+            manufacturer=self.manufacturer,
+            name=self.name)
 
-# class Battery(PowerSupply):
-#     """
-#     电瓶信息
-#     """
-#     voltage = models.FloatField(verbose_name="标称电压(V)")
-#     capacity = models.FloatField(verbose_name="容量(Ah)")
-#
-#
-# class Charger(PowerSupply):
-#     """
-#     电源
-#     """
-#     is_support_solar = models.BooleanField(verbose_name="是否支持太阳能")
-#     is_support_electricity = models.BooleanField(verbose_name="是否支持市电")
-#     is_support_battery = models.BooleanField(verbose_name="是否支持电瓶充电")
-#
-#
-# class SolarPanel(PowerSupply):
-#     """
-#     太阳能板
-#     """
-#     power = models.FloatField(verbose_name="功率")
-#     width = models.FloatField(verbose_name="宽(mm)")
-#     high = models.FloatField(verbose_name="高(mm)")
+    class Meta:
+        verbose_name = "供电设备型号"
+        verbose_name_plural = "供电设备型号"
+
+
+class NetworkDevice(Equipment):
+    ROUTER = "RT"
+    INTERCHANGER = "IC"
+    PHOTOCONVERTER = "PC"
+    FIREWALL = "FW"
+    OTHER = "OT"
+
+    NETWORK_TYPE = (
+        (ROUTER, "路由"),
+        (INTERCHANGER, "交换机"),
+        (PHOTOCONVERTER, "光电转换器"),
+        (FIREWALL, "防火墙"),
+        (OTHER, "其它"),
+    )
+    name = models.CharField(max_length=64, unique=True, verbose_name="型号")
+
+    type = models.CharField(choices=NETWORK_TYPE,
+                            max_length=2, default=ROUTER,
+                            verbose_name="网络设备类型")
+
+    # equipment = models.OneToOneField(Equipment, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "[{type}] {manufacturer}-{name}".format(
+            type=self.type,
+            manufacturer=self.manufacturer,
+            name=self.name)
+
+    class Meta:
+        verbose_name = "网络设备型号"
+        verbose_name_plural = "网络设备型号"
+
+
+class EquipmentEntity(models.Model):
+    """
+    设备实体共有信息
+    """
+    ENTITY_STATUS = (
+        (0, "库存"),
+        (1, "在线"),
+        (2, "故障"),
+        (3, "维修"),
+        (4, "报废"),
+        (5, "归还"),
+        (6, "其它"),
+    )
+
+    GETWAY_TYPE = (
+        (0, "自购"),
+        (1, "租赁"),
+        (2, "借用"),
+        (3, "其它"),
+    )
+
+    sn = models.CharField(max_length=32, verbose_name="编号")
+    status = models.PositiveSmallIntegerField(choices=ENTITY_STATUS,
+                                              default=0,
+                                              verbose_name="状态")
+
+    getway = models.PositiveSmallIntegerField(choices=GETWAY_TYPE,
+                                              default=0,
+                                              verbose_name="状态")
+
+    remark = models.TextField(blank=True, null=True, verbose_name="备注")
+    is_delete = models.BooleanField(default=False, verbose_name="已删除")
+
+
+class SensorEntity(EquipmentEntity):
+    """
+    测震仪实体信息
+    """
+    model = models.ForeignKey("SensorModel",
+                              on_delete=models.DO_NOTHING,
+                              verbose_name="地震仪型号")
+
+    # equipment_entity = models.OneToOneField("EquipmentEntity", on_delete=models.DO_NOTHING)
+    # equipment_entity = models.OneToOneField("EquipmentEntity", null=True, blank=True, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "[{model}] {sn}".format(
+            model=self.model,
+            sn=self.sn)
+
+    class Meta:
+        verbose_name = "地震仪实体"
+        verbose_name_plural = "地震仪实体"
+
+
+class DataloggerEntity(EquipmentEntity):
+    """
+    数采实体信息
+    """
+    model = models.ForeignKey("DataloggerModel",
+                              on_delete=models.DO_NOTHING,
+                              verbose_name="数采型号")
+
+    # equipment_entity = models.OneToOneField("EquipmentEntity", null=True, blank=True, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "[{model}] {sn}".format(
+            model=self.model,
+            sn=self.sn)
+
+    class Meta:
+        verbose_name = "数采实体"
+        verbose_name_plural = "数采实体"
